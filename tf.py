89086b7cb7b9abb4de84d63c61bccda9c38fb629
Wayne Simonsen <wayne.a.simonsen@gmail.com>
2021-02-24 14:33:56 -0600

    changed tf.py

--- tf.py
+++ tf.py
@@ -43,9 +43,9 @@ def get_free_port():
 def iptables_wrapper(action,nat_ip,port_to_forward=None,destination_port=None):
     if action and nat_ip:
         if port_to_forward and destination_port:
-            subprocess.run(['sudo', 'port_forwarding.sh', action, str(nat_ip), str(port_to_forward), str(destination_port)])
+            subprocess.run(['sudo', './port_forwarding.sh', action, str(nat_ip), str(port_to_forward), str(destination_port)])
         if not port_to_forward and not destination_port:
-            subprocess.run(['sudo', 'port_forwarding.sh', action, str(nat_ip)])
+            subprocess.run(['sudo', './port_forwarding.sh', action, str(nat_ip)])
 
 def start_vms(username, scenario_id):
     CWD = f'/home/wayne/projects/{scenario_id}/'
